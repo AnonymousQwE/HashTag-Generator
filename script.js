717024516f6f395df403d06dff74efce710f4cb2
AnonymousQwE <46275418+AnonymousQwE@users.noreply.github.com>
2022-12-17 10:57:26 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 let hashTag = "";
 
-btn.addEventListener("click", (str) => {
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
   hashTag = "#";
   HashtagGenerator(input.value);
   input.value = "";
 });
 
 function HashtagGenerator(str) {
-  console.log(str);
   str.split(" ").forEach((e) => {
     let string = e.split("");
     hashTag += string.shift().toUpperCase() + string.join("");
